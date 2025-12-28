15148b60c9b48d451326525c86e629c3ad6e4c5e
hiroshi <hiroshi@tanuki.page>
2025-12-27 19:44:21 -0500

    Pong: overlay: Build overlays for WallpaperPicker2
    There seems to be a bug with how the preview rounding is calculated for WallpaperPicker2 (Wallpaper & style) as well as the pixel variant of it after BP3A. This corrects it to better match the display.

--- device.mk
+++ device.mk
@@ -329,7 +329,9 @@ PRODUCT_PACKAGES += \
     NTSettingsProviderResTarget \
     NTSettingsResTarget \
     NTSystemUIResTarget \
-    NTWifiResTarget
+    NTWifiResTarget \
+    WallpaperPicker2PixelRes \
+    WallpaperPicker2Res
 
 # NCM overlays
 PRODUCT_PACKAGES += \
